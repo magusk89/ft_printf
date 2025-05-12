4c36eeabba92bd7009197d0d342d372a29cf1c56
Alex Barbosa Felix Da Silva <alebarbo@c1r7s4.42porto.com>
2025-05-12 15:59:11 +0100

    Second commit

--- ft_puthex.c
+++ ft_puthex.c
@@ -6,13 +6,13 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 22:55:50 by alebarbo          #+#    #+#             */
-/*   Updated: 2025/05/12 02:04:36 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/05/12 15:58:04 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_touphex(char *hex)
+/*static void	ft_touphex(char *hex)
 {
 	while (*hex)
 	{
@@ -20,29 +20,40 @@ static void	ft_touphex(char *hex)
 			*hex -= 32;
 		hex += 1;
 	}
-}
+}*/
 
-static int	ft_precision_print(const char *hex, t_flags *flags, char c)
+static int	ft_precision_print(const char *hex, t_flags *flags)
 {
 	int		char_counter;
 
 	char_counter = 0;
-	if (flags->bitflag & HASH || c == 'p')
+	while (flags->precision > ft_strlen(hex))
 	{
-		while (flags->precision > ft_strlen(hex) - 2)
+		char_counter += write(1, "0", 1);
+		flags->precision--;
+	}
+	return (char_counter);
+}
+
+static int	ft_flag_check(char *hex, t_flags *flags, char c)
+{
+	int		char_counter;
+	char	*tmp;
+
+	if (c == 'X')
+	{
+		tmp = hex;
+		while (*tmp)
 		{
-			char_counter += write(1, "0", 1);
-			flags->precision--;
+			if (*tmp >= 'a' && *tmp <= 'z')
+				*tmp -= 32;
+			tmp += 1;
 		}
 	}
+	if (flags->bitflag & HASH)
+		char_counter = ft_precision_print(hex + 2, flags);
 	else
-	{
-		while (flags->precision > ft_strlen(hex))
-		{
-			char_counter += write(1, "0", 1);
-			flags->precision--;
-		}
-	}
+		char_counter = ft_precision_print(hex, flags);
 	return (char_counter);
 }
 
@@ -65,7 +76,7 @@ static int	ft_width_print(const char *hex, t_flags *flags)
 	return (char_counter);
 }
 
-static int	ft_tohex(unsigned int nbr, t_flags *flags, const char c, char *hex)
+static int	ft_tohex(unsigned int nbr, t_flags *flags, char *hex)
 {
 	int		i;
 
@@ -80,14 +91,14 @@ static int	ft_tohex(unsigned int nbr, t_flags *flags, const char c, char *hex)
 		i--;
 	}
 	hex[i] = "0123456789abcdef"[nbr % 16];
-	if (flags->bitflag & HASH || c == 'p')
+	if (flags->bitflag & HASH)
 	{
-		hex[i - 1] = 'x';
 		hex[i - 2] = '0';
+		hex[i - 1] = 'x';
 		i -= 2;
 	}
-	if (c == 'X')
-		ft_touphex(&hex[i]);
+	/*if (c == 'X')
+		ft_touphex(&hex[i]);*/
 	return (i);
 }
 
@@ -98,12 +109,13 @@ int	ft_puthex(unsigned int nbr, t_flags *flags, const char conversion)
 	int		i;
 
 	char_counter = 0;
-	i = ft_tohex(nbr, flags, conversion, hex);
-	if (flags->precision > ft_strlen(&hex[i]))
+	i = ft_tohex(nbr, flags, hex);
+	if (flags->precision > ft_hexlen(nbr))
 		flags->width -= flags->precision - ft_strlen(&hex[i]);
 	if (flags->bitflag & DASH)
 	{
-		char_counter += ft_precision_print(&hex[i], flags, conversion);
+		char_counter += ft_flag_check(&hex[i], flags, conversion);
+		//char_counter += ft_precision_print(&hex[i], flags);
 		char_counter += write(1, hex, ft_strlen(&hex[i]));
 		char_counter += ft_width_print(&hex[i], flags);
 	}
@@ -115,7 +127,8 @@ int	ft_puthex(unsigned int nbr, t_flags *flags, const char conversion)
 	else
 	{
 		char_counter += ft_width_print(&hex[i], flags);
-		char_counter += ft_precision_print(&hex[i], flags, conversion);
+		char_counter += ft_flag_check(&hex[i], flags, conversion);
+		//char_counter += ft_precision_print(&hex[i], flags, conversion);
 		char_counter += write(1, &hex[i], ft_strlen(&hex[i]));
 	}
 	return (char_counter);
